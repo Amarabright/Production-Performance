4b5b14723a6d4b56046fa66bb59c522085679615
Amarachi Nwoke Ugagbe <83390581+Amarabright@users.noreply.github.com>
2023-08-31 22:09:12 +0100

    Add files via upload

---
+++ ProductionOptimizationQuery.sql
@@ -0,0 +1,12 @@
+
+SELECT * 
+  FROM [AdventureWorks2019].[Production].[Product]
+
+SELECT *
+  FROM [AdventureWorks2019].[Production].[BillOfMaterials]
+
+SELECT *
+	FROM [AdventureWorks2019].[Production].[WorkOrder]
+
+SELECT *
+	FROM [AdventureWorks2019].[Production].[WorkOrderRouting]
